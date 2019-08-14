cdb4cf5392cd6d5e90227d8abb1462bc47f2a984
Anderson Lima <50327795+anderdl18@users.noreply.github.com>
2019-08-14 10:26:36 -0300

    Add files via upload

---
+++ Aula02.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Aula02 {
+	
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		
+		float paoDoce = 1.20f, paoFrances = 0.00f;
+		int totalPaoFrances, totalPaoDoce;
+		
+		System.out.print("Informe a Quantidade de Pães Doces Vendidos: ");
+			totalPaodoce
+	}
+	}
